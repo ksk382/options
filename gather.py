7330ce65fe25e5debb745b02a24185ef322feeef
Kevin <ksk382@nyu.edu>
2021-01-31 20:31:33 -0800

    uncommenting the main functions

--- gather.py
+++ gather.py
@@ -82,43 +82,32 @@ def gather_stock_data():
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
 
-    existing_files = os.listdir(dir_name)
-    existing_files = [i for i in existing_files if i.endswith('.csv')]
-
-    done_tickers = []
-    now = dt.datetime.now()
-    for i in existing_files:
-        d = i.replace('.csv', '')
-        d = d[-len('2021-01-29 14.13'):]
-        done_ticker = i.replace(d, '').replace('_', '').replace('.csv', '')
-        prev_call_time = dt.datetime.strptime(d, "%Y-%m-%d %H.%M")
-        hour_ago = now - dt.timedelta(hours=1)
-        if prev_call_time > hour_ago:
-            done_tickers.append(done_ticker)
-
     error_list = []
     now_str = dt.datetime.strftime(now, "%Y-%m-%d %H.%M")
     err_output_file = '../logs/' + now_str + '_stock_error_list.txt'
     count = 0
+    all_df = pd.DataFrame()
+    fname = dt.datetime.strftime(now, "%Y-%m-%d %H.%M") + '.csv'
+    filename = dir_name + fname
+
     for ticker in ticker_list:
         count += 1
-        if ticker not in done_tickers:
-            now = dt.datetime.now()
-            try:
-                df = get_stock_df(ticker)
-                fname = ticker + '_' + dt.datetime.strftime(now, "%Y-%m-%d %H.%M") + '.csv'
-                filename = dir_name + fname
-                print (f'{count} writing {filename}')
-                df.to_csv(filename, compression='gzip', index=False)
-            except Exception as e:
-                print (ticker, str(e))
-                error_list.append([ticker, str(e)])
-                with open(err_output_file, 'a') as f:
-                    f.write("%s\n" % [ticker, str(e)])
-            # throttle API calls to 60 per minute
-            time.sleep(1)
-        else:
-            print (f'{count} - {ticker} already done in last hour')
+        try:
+            df = get_stock_df(ticker)
+            all_df = all_df.append(df, ignore_index = True)
+            print (f'{count} retrieved {ticker}')
+            if count % 50 == 0:
+                print (f'{count} writing to {filename}')
+                all_df.to_csv(filename, compression='gzip', index=False)
+        except Exception as e:
+            print (ticker, str(e))
+            error_list.append([ticker, str(e)])
+            with open(err_output_file, 'a') as f:
+                f.write("%s\n" % [ticker, str(e)])
+        # throttle API calls to 60 per minute
+        time.sleep(1)
+
+    all_df.to_csv(filename, compression='gzip', index=False)
 
     print ('-----done gathering stock data')
 
@@ -138,47 +127,37 @@ def gather_etf_data():
     if not os.path.exists(dir_name):
         os.mkdir(dir_name)
 
-    existing_files = os.listdir(dir_name)
-    existing_files = [i for i in existing_files if i.endswith('.csv')]
-
-    done_tickers = []
-    now = dt.datetime.now()
-    for i in existing_files:
-        d = i.replace('.csv', '')
-        d = d[-len('2021-01-29 14.13'):]
-        done_ticker = i.replace(d, '').replace('_', '').replace('.csv', '')
-        prev_call_time = dt.datetime.strptime(d, "%Y-%m-%d %H.%M")
-        hour_ago = now - dt.timedelta(hours=1)
-        if prev_call_time > hour_ago:
-            done_tickers.append(done_ticker)
-
     error_list = []
     now_str = dt.datetime.strftime(now, "%Y-%m-%d %H.%M")
-    err_output_file = '../logs/' + now_str + '_etf_error_list.txt'
+    err_output_file = '../logs/' + now_str + '_stock_error_list.txt'
     count = 0
+    all_df = pd.DataFrame()
+    fname = dt.datetime.strftime(now, "%Y-%m-%d %H.%M") + '.csv'
+    filename = dir_name + fname
+
     for ticker in ticker_list:
         count += 1
-        if ticker not in done_tickers:
-            now = dt.datetime.now()
-            try:
-                df = get_stock_df(ticker)
-                fname = ticker + '_' + dt.datetime.strftime(now, "%Y-%m-%d %H.%M") + '.csv'
-                filename = dir_name + fname
-                print (f'{count} writing {filename}')
-                df.to_csv(filename, compression='gzip', index=False)
-            except Exception as e:
-                print (ticker, str(e))
-                error_list.append([ticker, str(e)])
-                with open(err_output_file, 'a') as f:
-                    f.write("%s\n" % [ticker, str(e)])
-            # throttle API calls to 60 per minute
-            time.sleep(1)
-        else:
-            print (f'{count} - {ticker} already done in last hour')
+        try:
+            df = get_stock_df(ticker)
+            all_df = all_df.append(df, ignore_index=True)
+            print (f'{count} retrieved {ticker}')
+            if count % 50 == 0:
+                print (f'{count} writing to {filename}')
+                all_df.to_csv(filename, compression='gzip', index=False)
+        except Exception as e:
+            print (ticker, str(e))
+            error_list.append([ticker, str(e)])
+            with open(err_output_file, 'a') as f:
+                f.write("%s\n" % [ticker, str(e)])
+        # throttle API calls to 60 per minute
+        time.sleep(1)
 
-    print ('-----done gathering stock data')
+    all_df.to_csv(filename, compression='gzip', index=False)
+
+    print ('-----done gathering etf data')
+
 
 if __name__ == "__main__":
-    gather_option_data()
+    #gather_option_data()
     gather_stock_data()
     gather_etf_data()
--- testing.py
+++ testing.py
@@ -5,5 +5,26 @@ from io import StringIO
 import xml.etree.ElementTree as ET
 import pandas as pd
 import datetime as dt
+import os
 
+now = dt.datetime.now() - dt.timedelta(days=1)
+print (now)
+today = str(now)[:10]
+print (today)
+dir_name = '../stock_dataframes/' + today + '/'
+pd.set_option('display.max_rows', 500)
 
+x = os.listdir(dir_name)
+a = pd.DataFrame()
+for i in x:
+    if i.endswith('.csv'):
+        j = dir_name + i
+        df = pd.read_csv(j, compression = 'gzip')
+        df['Ticker'] = i.split('_')[0]
+        print (df)
+        print (df.columns)
+        print (df.loc[0])
+        input('enter')
+        a = a.append(df, ignore_index = True)
+print (a)
+a.to_csv('all.csv', compression = 'gzip', index = False)
