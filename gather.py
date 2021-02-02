2c66efdecdf802be7b1c31968bc6cf2d3c6d7a01
Kevin <ksk382@nyu.edu>
2021-02-02 13:34:59 -0800

    adding functionality to allow you to pick up midstream on a terminated run

--- gather.py
+++ gather.py
@@ -23,10 +23,16 @@ def gather_stock_and_option_data():
     print (now)
     if not os.path.exists(stock_dir_name):
         os.mkdir(stock_dir_name)
-    all_stock_df = pd.DataFrame()
     now_str = dt.datetime.strftime(now, "%Y-%m-%d_%H.%M")
+    # or you can manually pick up an unfinished pull
+    # now_str = '2021-02-02_16.19'
     fname = now_str + '.csv'
     stock_save_name = stock_dir_name + fname
+    if os.path.isfile(stock_save_name):
+        all_stock_df = pd.read_csv(stock_save_name, compression='gzip')
+    else:
+        all_stock_df = pd.DataFrame()
+        all_stock_df['symbol'] = ''
 
     # initialize option save folder
     option_dir_name = '../option_dataframes/' + now_str + '/'
@@ -44,48 +50,52 @@ def gather_stock_and_option_data():
     s = pd.DataFrame([])
     exception_count = 0
     for ticker in ticker_list:
-        try:
-            count += 1
-            stock_df = get_stock_df(ticker)
-            s = s.append({'call_time': time.time()}, ignore_index=True)
-            print (f'{count} retrieved {ticker} stock')
+        count += 1
+        print (f'{count} getting {ticker} data')
+        if ticker in all_stock_df['symbol'].unique():
+            print (f'Already have {ticker}')
+        else:
+            try:
+                stock_df = get_stock_df(ticker)
+                s = s.append({'call_time': time.time()}, ignore_index=True)
+                print (f'{count} retrieved {ticker} stock')
 
-            option_df = get_option_df(ticker)
-            s = s.append({'call_time': time.time()}, ignore_index=True)
-            print (f'{count} retrieved {ticker} option chain')
+                option_df = get_option_df(ticker)
+                s = s.append({'call_time': time.time()}, ignore_index=True)
+                print (f'{count} retrieved {ticker} option chain')
 
-            all_stock_df = all_stock_df.append(stock_df, ignore_index=True)
+                all_stock_df = all_stock_df.append(stock_df, ignore_index=True)
 
-            # save every 100 tickers
-            if count % 100 == 0:
-                print (f'{count} writing to {stock_save_name}')
-                all_stock_df.to_csv(stock_save_name, compression='gzip', index=False)
+                # save every 100 tickers
+                if count % 100 == 0:
+                    print (f'{count} writing to {stock_save_name}')
+                    all_stock_df.to_csv(stock_save_name, compression='gzip', index=False)
 
-            option_save_name = option_dir_name + ticker + '_.csv'
-            print (f'{count} writing to {option_save_name}')
-            option_df.to_csv(option_save_name, compression='gzip', index=False)
+                option_save_name = option_dir_name + ticker + '_.csv'
+                print (f'{count} writing to {option_save_name}')
+                option_df.to_csv(option_save_name, compression='gzip', index=False)
 
-            # rate limiting
-            y = time.time() - 60
-            q = s[s['call_time'] > y]
-            breaker = 0
-            while len(q) > 58 or breaker > 10:
-                sleep_time = (len(q) - 58)
-                print (f'number of calls in last minute: {len(q)} ---- sleeping {sleep_time} seconds')
-                time.sleep(sleep_time)
-                y = time.time()
+                # rate limiting
+                y = time.time() - 60
                 q = s[s['call_time'] > y]
-                breaker +=1
-            exception_count = 0
+                breaker = 0
+                while len(q) > 58 or breaker > 10:
+                    sleep_time = (len(q) - 58)
+                    print (f'number of calls in last minute: {len(q)} ---- sleeping {sleep_time} seconds')
+                    time.sleep(sleep_time)
+                    y = time.time()
+                    q = s[s['call_time'] > y]
+                    breaker +=1
+                exception_count = 0
 
-        except Exception as e:
-            exception_count += 1
-            print (ticker, str(e))
-            error_list.append([ticker, str(e)])
-            with open(err_output_file, 'a') as f:
-                f.write("%s\n" % [ticker, str(e)])
-            if exception_count > 2:
-                time.sleep(2)
+            except Exception as e:
+                exception_count += 1
+                print (ticker, str(e))
+                error_list.append([ticker, str(e)])
+                with open(err_output_file, 'a') as f:
+                    f.write("%s\n" % [ticker, str(e)])
+                if exception_count > 2:
+                    time.sleep(2)
 
     print (f'{count} writing to {stock_save_name}')
     all_stock_df.to_csv(stock_save_name, compression='gzip', index=False)
