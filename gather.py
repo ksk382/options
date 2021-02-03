f80fea009f874e0a585da8502f01c9b677547b1a
Kevin <ksk382@nyu.edu>
2021-02-03 15:38:14 -0800

    fixing main gaither function

--- gather.py
+++ gather.py
@@ -6,7 +6,7 @@ import time
 from nope import run_nope
 
 
-def gather_stock_and_option_data():
+def gather_stock_and_option_data(**kwargs):
     print ('\n\n\n\n&&&&&----- gathering stock and option data\n\n')
 
     # gather the tickers to use
@@ -17,15 +17,35 @@ def gather_stock_and_option_data():
     print ('ticker list:')
     print (ticker_list)
 
-    # initialize stock dataframe
+    if 'kow_str' in kwargs.keys:
+        now_str = kwargs['kow_str']
+    else:
+        now = dt.datetime.now()
+        now_str = dt.datetime.strftime(now, "%Y-%m-%d_%H.%M")
+        print (now_str)
+
+
+    print ('&&&&&----- beginning loop')
+
+    error_list = run_loop(ticker_list, now_str)
+
+    retry_list = []
+    for i in error_list:
+        retry_list.append(i[0])
+    error_list_2 = run_loop(retry_list, now_str)
+
+    print ('error_list: ')
+    for i in error_list_2:
+        print (i)
+    print (f'{len(error_list)} errors')
+
+    print ('\n\n\n\n&&&&&----- done gathering stock and option data\n\n')
+
+    return
+
+def run_loop(ticker_list, now_str):
+
     stock_dir_name = '../stock_dataframes/'
-    now = dt.datetime.now()
-    print (now)
-    if not os.path.exists(stock_dir_name):
-        os.mkdir(stock_dir_name)
-    now_str = dt.datetime.strftime(now, "%Y-%m-%d_%H.%M")
-    # or you can manually pick up an unfinished pull
-    # now_str = '2021-02-02_16.19'
     fname = now_str + '.csv'
     stock_save_name = stock_dir_name + fname
     if os.path.isfile(stock_save_name):
@@ -41,13 +61,8 @@ def gather_stock_and_option_data():
     if not os.path.exists(option_dir_name):
         os.mkdir(option_dir_name)
 
-    # initialize error logging
-    err_output_file = '../logs/' + now_str + '_stock_error_list.txt'
     error_list = []
-
     count = 0
-    # set up a rate limiter
-    s = pd.DataFrame([])
     exception_count = 0
     for ticker in ticker_list:
         count += 1
@@ -61,7 +76,6 @@ def gather_stock_and_option_data():
                 print (f'{count} of {len(ticker_list)} retrieved {ticker} stock')
 
                 option_df, option_rate_remaining = get_option_df(ticker)
-                s = s.append({'call_time': time.time()}, ignore_index=True)
                 print (f'{count} of {len(ticker_list)} retrieved {ticker} option chain')
 
                 all_stock_df = all_stock_df.append(stock_df, ignore_index=True)
@@ -85,43 +99,20 @@ def gather_stock_and_option_data():
                     print (f'{count} of {len(ticker_list)} rate_remaining: {rate_remaining} ---- sleeping {sleep_time} seconds')
                     time.sleep(sleep_time)
 
-                '''
-                # alternate rate limiting code
-                y = time.time() - 60
-                q = s[s['call_time'] > y]
-                breaker = 0
-                while len(q) > 58 or breaker > 10:
-                    sleep_time = (len(q) - 58)
-                    print (f'number of calls in last minute: {len(q)} ---- sleeping {sleep_time} seconds')
-                    time.sleep(sleep_time)
-                    y = time.time()
-                    q = s[s['call_time'] > y]
-                    breaker +=1
-                exception_count = 0
-                '''
-
             except Exception as e:
                 exception_count += 1
                 print (ticker, str(e))
                 error_list.append([ticker, str(e)])
-                with open(err_output_file, 'a') as f:
-                    f.write("%s\n" % [ticker, str(e)])
                 if exception_count > 2:
                     time.sleep(2)
 
     print (f'{count} writing to {stock_save_name}')
     all_stock_df.to_csv(stock_save_name, compression='gzip', index=False)
 
-    print ('error_list: ')
-    for i in error_list:
-        print (i)
-    print (f'{len(error_list)} errors')
+    return error_list
 
-    print ('\n\n\n\n&&&&&----- done gathering stock and option data\n\n')
-
-    return
 
 if __name__ == "__main__":
-
-    gather_stock_and_option_data()
+    now_str = '2021-02-03_15.30'
+    gather_stock_and_option_data(kow_str = now_str)
     run_nope()
