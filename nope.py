5aafe20048407e1756d2aee72708b600903ce7cc
Kevin <ksk382@nyu.edu>
2021-02-07 14:41:30 -0800

    adding data to nope files

--- nope.py
+++ nope.py
@@ -146,7 +146,7 @@ def run_nope(**kwargs):
 
         print (a.items())
         print (a.keys())
-        temp_df = pd.DataFrame(a.items(), columns = a.keys())
+        temp_df = pd.DataFrame(a.values(), columns = a.keys())
         for g in greeks:
             o_name = f'mw_{g}'
             temp_df[o_name] = option_df[o_name].mean()
