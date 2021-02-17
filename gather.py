13fb3a649415e3942db33996a80fada5cf5a60c2
Kevin <ksk382@nyu.edu>
2021-02-17 07:51:41 -0800

    short yfmerge

--- gather.py
+++ gather.py
@@ -5,7 +5,6 @@ import os
 import time
 import sys
 import argparse
-from yf_retrieve import yf_merge
 
 
 def gather_stock_and_option_data(**kwargs):
@@ -227,4 +226,4 @@ if __name__ == "__main__":
         gather_stock_and_option_data()
     else:
         gather_stock_data()
-        yf_merge()
+
