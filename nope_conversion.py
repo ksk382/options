922006c16db48dff2c2f66a6759e93fa6ac400fe
Kevin <ksk382@nyu.edu>
2021-04-03 10:00:05 -0700

    nope_conversion.py

--- nope_conversion.py
+++ nope_conversion.py
@@ -5,8 +5,11 @@ import pandas as pd
 third_dir = '../nope_dataframes/'
 flist = [(third_dir + i) for i in os.listdir(third_dir) if i.endswith('_nope.csv')]
 for i in flist:
-    df = pd.read_csv(i, compression = 'gzip')
-    df = df.rename({'ticker': 'symbol'}, axis=1)
-    df.to_csv(i, compression='gzip', index=False)
     print (i)
+    try:
+        df = pd.read_csv(i, compression = 'gzip')
+        df = df.rename({'ticker': 'symbol'}, axis=1)
+        df.to_csv(i, compression='gzip', index=False)
+    except Exception as e:
+        print (str(e))
 print('done')
